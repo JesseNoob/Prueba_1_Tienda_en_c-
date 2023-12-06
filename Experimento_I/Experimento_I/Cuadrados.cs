7cba7f91ca1dee6f60c3bb41b9b03a3324a7786b
Jesse <103623437+JesseNoob@users.noreply.github.com>
2023-12-05 20:38:20 -0600

    Subir archivos

---
+++ Experimento_I/Experimento_I/Cuadrados.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace Experimento_I
+{
+    internal class Cuadrados
+    {
+        //1) Para dibujar los cuadrados se necesitan dos cosas, las coordenadas de su esquina superior izquierda y las de su esquina inferior derecha
+        //   apartir de ahi, un método dibujara los bordes
+        public Point EsquinaSuperiorIzquierda { get; set;} //Esta propiedad recibe dos valores, (x) e (y), no hacer caso al get o al set... Eso es para otra cosa
+        public Point EsquinaInferiorDerecha { get; set;}
+        //2) Para recibir los datos se emplea la función constructora que define el estado inicial de los objetos (Manipula las propiedades)
+        public Cuadrados(Point esquinaSuperiorIzquierda, Point esquinInferiorDerecha)//Utilizo parametros para almacenar los valores que ingresaria desde otra clase mientras hago algun objeto
+        {
+            EsquinaSuperiorIzquierda = esquinaSuperiorIzquierda;//Esos valores se terminan guardando en la propiedades que defini arriba
+            EsquinaInferiorDerecha = esquinInferiorDerecha;//De este modo podré acceder a su valores
+        }
+        public void DibujarBorde()
+        {
+            //Dibujando en el eje x
+            for(int i = EsquinaSuperiorIzquierda.X; i <= EsquinaInferiorDerecha.X; i++)
+            {
+                Console.SetCursorPosition(i, EsquinaSuperiorIzquierda.Y);
+                Console.Write("═");//Alt + 205
+                Console.SetCursorPosition(i, EsquinaInferiorDerecha.Y);
+                Console.Write("═");//Alt + 205
+            }
+            //Dibujando en el Y
+            for (int i = EsquinaSuperiorIzquierda.Y; i <= EsquinaInferiorDerecha.Y; i++)
+            {
+                Console.SetCursorPosition(EsquinaSuperiorIzquierda.X, i);
+                Console.Write("║");//Alt + 186
+                Console.SetCursorPosition(EsquinaInferiorDerecha.X, i);
+                Console.Write("║");//Alt + 186
+            }
+            Console.SetCursorPosition(EsquinaSuperiorIzquierda.X, EsquinaSuperiorIzquierda.Y);
+            Console.Write("╔");//Alt + 201
+            Console.SetCursorPosition(EsquinaSuperiorIzquierda.X, EsquinaInferiorDerecha.Y);
+            Console.Write("╚");//Alt + 200
+            Console.SetCursorPosition(EsquinaInferiorDerecha.X, EsquinaSuperiorIzquierda.Y);
+            Console.Write("╗");//Alt + 187
+            Console.SetCursorPosition(EsquinaInferiorDerecha.X, EsquinaInferiorDerecha.Y);
+            Console.Write("╝");//Alt + 188
+        }
+
+
+
+
+    }
+}
---
+++ Experimento_I/Experimento_I/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+//using Experimento_I;
+namespace Experimento_I
+{
+    class Principal
+    {
+        public static void DibujarInterfaz()
+        {
+
+        }
+        public static void EscribirContenido()
+        {
+            Console.SetCursorPosition(10, 8);
+            Console.WriteLine("------------TIENDA SUPER HELLO----------------");
+            Console.SetCursorPosition(30, 22);
+            Console.Write("PlayStation");
+            Console.SetCursorPosition(75, 22);
+            Console.Write("Xbox");
+            Console.SetCursorPosition(120, 22);
+            Console.Write("Nintendo");
+            Console.SetCursorPosition(10, 34);
+            Console.Write("Ingrese su marca favorita: ");
+            Console.SetCursorPosition(10, 36);
+        }
+        public static void EscribirPresentacion()
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(70,25);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("¡¡Bienvenido a la tienda de consolas!!");
+            Console.ResetColor();
+            
+        }
+        public static void Main()
+        {
+            string opcion;
+            Ventana ventana = new Ventana(180, 50);
+            Cuadrados marcoDeLaVentana = new Cuadrados(new Point(2, 2), new Point(174, 50));
+            Cuadrados primerMenu = new Cuadrados(new Point(10, 10), new Point(165, 30));
+            Cuadrados tituloPlayStation = new Cuadrados(new Point(15, 20), new Point(55, 25));
+            Cuadrados tituloXbox = new Cuadrados(new Point(60, 20), new Point(100, 25));
+            Cuadrados tituloNintendo = new Cuadrados(new Point(105, 20), new Point(145, 25));
+/*******************************************Primer Pantalla(Presentacion)**********************************************************/
+            marcoDeLaVentana.DibujarBorde();
+            EscribirPresentacion();
+            Thread.Sleep(9000);
+            Console.Clear();
+/*******************************************Segunda Pantalla**********************************************************/
+            marcoDeLaVentana.DibujarBorde();
+            primerMenu.DibujarBorde();
+            tituloPlayStation.DibujarBorde();
+            tituloXbox.DibujarBorde();
+            tituloNintendo.DibujarBorde();
+            EscribirContenido();
+            opcion = Console.ReadLine().ToLower();
+            Console.Clear();
+            Console.SetCursorPosition(80,25);
+            switch (opcion)
+            {
+                case "playstation":
+
+                    Console.ForegroundColor= ConsoleColor.DarkCyan;
+                    Console.WriteLine("Haz elgido la primera opcion");
+                    break;
+                case "xbox":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Haz elgido la segunda opcion");
+                    break;
+                case "nintendo":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Haz elgido la tercera opcion");
+                    break;
+                default:
+                    Console.WriteLine("No existe esa ocpcion");
+                    break;
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
